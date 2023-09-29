d8c80ab9376f96176f29483b4e9fcf10dd96bb34
peterph6 <Peter.phibbs@sydney.edu.au>
2023-09-29 16:24:42 +1000

    hello very tiny word

--- GitTest10/ContentView.swift
+++ GitTest10/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, tiny  world!")
+            Text("Hello, vbery tiny  world!")
         }
         .padding()
     }
