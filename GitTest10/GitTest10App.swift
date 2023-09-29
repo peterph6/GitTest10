b198ca00e2805431c7bea8c7c542f8754edd25dd
peterph6 <Peter.phibbs@sydney.edu.au>
2023-09-29 16:21:36 +1000

    Initial Commit

---
+++ GitTest10/GitTest10App.swift
@@ -0,0 +1,17 @@
+//
+//  GitTest10App.swift
+//  GitTest10
+//
+//  Created by Peter Phibbs on 29/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitTest10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
